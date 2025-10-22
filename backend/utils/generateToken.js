1e89818ecf5463c5a64fd50024f7f5639d97774e
Subhajit Ghosh <gamingsubha07@gmail.com>
2025-10-22 21:00:35 +0530

    fix the generateToken

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,18 +5,18 @@ const generateTokenAndSetCookie = (userId, res) => {
         expiresIn: "15d",
     });
 
-    // res.cookie("token", token, {
-    //     maxAge: 15 * 24 * 60 * 60 * 1000, 
-    //     httpOnly: true,
-    //     sameSite: 'none',
-    //     secure: true,
-    // });
-        res.cookie("token", token, {
-        maxAge: 15 * 24 * 60 * 60 * 1000,
+    res.cookie("token", token, {
+        maxAge: 15 * 24 * 60 * 60 * 1000, 
         httpOnly: true,
-        sameSite: 'lax',
-        secure: false,
-});
+        sameSite: 'none',
+        secure: true,
+    });
+        // res.cookie("token", token, {
+        // maxAge: 15 * 24 * 60 * 60 * 1000,
+        // httpOnly: true,
+        // sameSite: 'lax',
+        // secure: false,
+        // });
 
 };
 
